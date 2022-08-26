8b9269d5fcaaf12c266ab2bf054ab18e71f18875
unkler <poru@add-natural.com>
2022-08-26 12:48:25 +0900

    フォロー機能とユーザーページ作成

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use App\Mail\BareMail;
 use App\Notifications\PasswordResetNotification;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -43,4 +45,43 @@ class User extends Authenticatable
     {
         $this->notify(new PasswordResetNOtification($token, new BareMail()));
     }
+
+    public function articles(): HasMany
+    {
+        return $this->hasMany('App\Models\Article');
+    }
+    
+
+    public function followers(): BelongsToMany
+    {
+        return $this->belongsToMany('App\Models\User', 'follows', 'followee_id', 'follower_id')->withTimestamps();
+    }
+
+    public function followings(): BelongsToMany
+    {
+        return $this->belongsToMany('App\Models\User', 'follows', 'follower_id', 'followee_id')->withTimestamps();
+    }
+
+    public function likes(): BelongsToMany
+    {
+        return $this->belongsToMany('App\Models\Article', 'likes')->withTimestamps();
+    }
+
+    public function isFollowedBy(?User $user): bool
+    {
+        return $user
+            ? (bool)$this->followers->where('id', $user->id)->count()
+            : false;
+    }
+
+    public function getCountFollowersAttribute(): int
+    {
+        return $this->followers()->count();
+    }
+
+    public function getCountFollowingsAttribute(): int
+    {
+        return $this->followings()->count();
+    }
+
 }
